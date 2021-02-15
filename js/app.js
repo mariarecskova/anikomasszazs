f47ee3e8958e73f0e8407efeaa586e59051b387c
Maria <maria.recska@gmail.com>
2021-02-15 19:03:04 +0100

    navbar and carousel ready, smal fixes needed

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".navbar-links");
+  const navLinks = document.querySelectorAll(".navbar-links li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards 
+        ${index / 7 + 0.3 }s`;
+    }
+    });
+  });
+};
+navSlide();
